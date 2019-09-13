11e2cac2124bf45eeb194222cbefcddbe6a9b912
Ubuntu <student@ML-RefVm-771511.fijbcecgaylu3jicltlyagxudg.qx.internal.cloudapp.net>
2019-09-13 03:27:17 +0000

    script for create a new folder into existing directory

--- portfolio/week2/foldermaker.sh
+++ portfolio/week2/foldermaker.sh
@@ -2,5 +2,3 @@
 read -p "type the name of the folder you would like to create:" folderName
 # According to standard input create a new folder.
 mkdir "$folderName"
-# List all the contents of the folder.
-ls ~/CSI6203/portfolio/week2/"$folderName"
